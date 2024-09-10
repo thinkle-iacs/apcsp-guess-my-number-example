c8039dba879792f48614232202db88443ccd866a
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-09-10 11:47:48 +0000

    First change -- added a word for a test.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { TextInterface } from "text-interface";
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Example Text Interface");
-ti.output("What is your name?");
+ti.output("What is your name, buddy?");
 let name = await ti.readText();
 ti.output("Hello, " + name);
 
